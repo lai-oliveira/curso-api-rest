d48d828c2fc137ca17e845111638a070ba8f238d
lai-oliveira <lai_cunha@outlook.com>
2022-12-17 23:07:27 -0300

    interface user repository

---
+++ src/main/java/br/com/projetorest/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.projetorest.api.repositories;
+
+
+import br.com.projetorest.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
