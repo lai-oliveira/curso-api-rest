86f3ce71e385f4a6fd7af307b38357dacdf65ab6
lai-oliveira <lai_cunha@outlook.com>
2022-12-18 00:29:27 -0300

    criando o repository

--- src/main/java/br/com/projetorest/api/repositories/UserRepository.java
+++ src/main/java/br/com/projetorest/api/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package br.com.projetorest.api.repositories;
 
 
-import br.com.projetorest.api.domain.User;
+import br.com.projetorest.api.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
 }
