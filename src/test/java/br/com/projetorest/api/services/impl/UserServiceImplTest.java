ba488e486f656bc4e0ace8184fc7456af18c25e7
lai-oliveira <lai_cunha@outlook.com>
2022-12-19 01:45:51 -0300

    teste para a classe user service metodo update com excecao

--- src/test/java/br/com/projetorest/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/projetorest/api/services/impl/UserServiceImplTest.java
@@ -116,8 +116,20 @@ class UserServiceImplTest {
 
     }
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(users);
+
+        Users response = service.update(userDto);
+
+
+        assertNotNull(response);
+        assertEquals(Users.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
+
 
     @Test
     void delete() {
