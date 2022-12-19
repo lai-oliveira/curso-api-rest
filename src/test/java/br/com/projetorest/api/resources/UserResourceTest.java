b73afb7aac5f1fe94ea49a5f9212174d8d9d736a
lai-oliveira <lai_cunha@outlook.com>
2022-12-19 20:30:07 -0300

    teste para endpoint create  da classe user resource implementado

--- src/test/java/br/com/projetorest/api/resources/UserResourceTest.java
+++ src/test/java/br/com/projetorest/api/resources/UserResourceTest.java
@@ -91,7 +91,13 @@ class UserResourceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+        when(service.create(any())).thenReturn(users);
+        ResponseEntity<UserDto> response = resource.create(userDto);
+
+        assertEquals(ResponseEntity.class,response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
